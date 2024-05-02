b0940390b2a2a1ce3a66e058f5cbfda92d6b50d1
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-05-02 21:42:35 +0100

    Setup theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      "inter": ["Inter", "sans-serif"],
+    },
+    colors: {
+      "green": "hsl(75, 94%, 57%)",
+      "white": "hsl(0, 0%, 100%)",
+      "grey": "hsl(0, 0%, 20%)",
+      "dark-grey": "hsl(0, 0%, 12%)",
+      "off-black": "hsl(0, 0%, 8%)"
+    },
     extend: {},
   },
   plugins: [],
